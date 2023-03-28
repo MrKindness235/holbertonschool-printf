7ae03379c436b9c6b0f1806361ee86ff9f215637
MrKindness235 <patriciocoitino235@gmail.com>
2023-03-28 07:26:13 -0700

    Fresh new repository to begin working in the Printf project. Such exciment I can feel and the weight of many hours seldom ever slept.

---
+++ README.md
@@ -0,0 +1,1 @@
+This is the repository where all printf's proyect files will reside.
---
+++ main.h
@@ -0,0 +1,9 @@
+#ifndef _FILE_NAME_H_
+#define _FILE_NAME_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+#endif
