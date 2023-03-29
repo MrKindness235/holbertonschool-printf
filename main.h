e078cec2655ffb72989e355379166ca7422f78a7
ELCBAh <2611@holbertonstudents.com>
2023-03-29 07:28:05 -0700

    Fixed structure definition

--- main.h
+++ main.h
@@ -10,12 +10,12 @@
  * @i: input %i
  * @d: input %d
  */
-typedef struct formatCheck
+struct formatCheck check;
 {
 	const char *format;
 	int num;
 	char *charInput;
-} check;
+};
 char _charprinter(char *);
 int _numprinter(int);
 int _printf(const char *format, ...);
